24f9a81450a176711159cd35c347ec467a1aa0f6
rodolfoblancato <rodolfoblancato@gmail.com>
2021-06-18 11:09:11 -0300

    Adicionada função para criar um rodapé na imagem

--- auto_scrshot.py
+++ auto_scrshot.py
@@ -1,6 +1,15 @@
 import subprocess
 from PIL import Image, ImageDraw
 
+'''Funções '''
+def add_margin(pil_img, top, right, bottom, left, color):
+    width, height = pil_img.size
+    new_width = width + right + left
+    new_height = height + top + bottom
+    result = Image.new(pil_img.mode, (new_width, new_height), color)
+    result.paste(pil_img, (left, top))
+    return result
+
 ''' Passo 1 - Ler o arquivo txt com a lista de sites'''
 
 ''' Passo 2 - Gerar os screenshots'''
@@ -10,10 +19,7 @@ output_file = '"C:/Users/rodol/Downloads/print_01.png"'
 subprocess.call('"C:/Program Files/Google/Chrome/Application/chrome.exe" --headless --screenshot=' + output_file + ' --hide-scrollbars --window-size=1366,2000 ' + website)
 
 ''' Passo 3 - Inserir data e hora nos screenshots''' 
-with Image.open("C:/Users/rodol/Downloads/print_01.png") as img1:
-    img2 = img1.crop( (0,-34,1366,2000) )  
-    draw = ImageDraw.Draw(img2)
-    draw.rectangle( (0,0,1366,34), fill="white" )
-    draw.rectangle( (0,2000,1366,1366), fill="white" )
-    del draw
-    img2.save("C:/Users/rodol/Downloads/img2.png", "PNG", quality=75)
+
+with Image.open("C:/Users/rodol/Downloads/print_01.png") as im:
+	im_new = add_margin(im, 0, 0, 100, 0, (0, 0, 0))
+	im_new.save('C:/Users/rodol/Downloads/print_rodape_01.png', quality=95)
