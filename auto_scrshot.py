231004440414ea61b7d97274578b51b1afaac029
rodolfoblancato <rodolfoblancato@gmail.com>
2021-06-18 13:08:14 -0300

    Ajustes no código para permitir a criação dos screenshots a partir de arquivo TXT com a lista de websites

--- auto_scrshot.py
+++ auto_scrshot.py
@@ -1,7 +1,7 @@
 import subprocess
 from PIL import Image, ImageDraw
 
-''' Adiciona uma margem à imagem '''
+''' Adiciona uma margem à imagem'''
 def add_margin(pil_img, top, right, bottom, left, color):
     width, height = pil_img.size
     new_width = width + right + left
@@ -11,22 +11,34 @@ def add_margin(pil_img, top, right, bottom, left, color):
     return result
 
 ''' Gera um screenshot do site'''
-def create_screenshot(website, output_file):
-	print('Fazendo screenshot do site 1')
-	subprocess.call('"C:/Program Files/Google/Chrome/Application/chrome.exe" --headless --screenshot="' + output_file + '" --hide-scrollbars --window-size=1366,2000 "' + website + '"')
-	print('Criado screenshot do site 1')
+def create_screenshot(website, output_path, file_name, counter):
+	print('Fazendo screenshot do site ' + str(counter))
+	subprocess.call('"C:/Program Files/Google/Chrome/Application/chrome.exe" --headless --screenshot="' + output_path + file_name + '" --hide-scrollbars --window-size=1366,2000 "' + website + '"')
+	print('Criado screenshot do site ' + str(counter))
 
 
 ''' Insere data e hora nos screenshots''' 
-def create_header(screenshot_file):
-	with Image.open(screenshot_file) as im:
-		print('Gerando cabeçalho do screenshot 1')
-		im_new = add_margin(im, 100, 0, 0, 0, (255, 255, 255))
-		im_new.save('C:/Users/rodol/Downloads/print_edit_01.png', quality=95)
-		print('Finalizado cabeçalho do screenshot 1')
+def create_header(output_path, file_name, counter):
+	with Image.open(output_path + file_name) as im:
+		print('Gerando cabeçalho do screenshot ' + str(counter))
+		new_name = 'edit_' + file_name
+		im_new = add_margin(im, 100, 0, 0, 0, (100, 100, 100))
+		im_new.save(output_path + new_name, quality=95)
+		print('Finalizado cabeçalho do screenshot ' + str(counter))
 
 ''' Ler o arquivo txt com a lista de sites e chamar as funções'''
-website = 'https://www.folha.uol.com.br/'
-file_path = 'C:/Users/rodol/Downloads/print_01.png'
-create_screenshot(website, file_path)
-create_header(file_path)
+def loop_websites(websites_list):
+	counter = 1
+	for website in websites_list:
+		file_name = 'screenshot_' + str(counter) + '.png'
+		create_screenshot(website, output_path, file_name, counter)
+		create_header(output_path, file_name, counter)
+		counter += 1
+
+output_path = 'C:/Users/rodol/Downloads/'
+input_file = input('Arquivo TXT com a lista de websites (deve ser incluído o caminho completo e a extensão):')
+websites = []
+with open(input_file) as websites_file:
+	for line in websites_file:
+		websites.append(line)
+loop_websites(websites)
